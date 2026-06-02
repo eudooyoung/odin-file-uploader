198e890259077cc84b875e7122ba5ae8c30bc4fb
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-06-02 11:44:50 +0900

    feat: add prisma instance and ca.pem

---
+++ src/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client.js";
+import config from "../config/env.config.js";
+
+const connectionString = config.databaseUrl;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
