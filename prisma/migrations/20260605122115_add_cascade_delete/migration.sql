08272283e88ddeb72b55ecb552db2309b3c961ac
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-06-05 21:21:50 +0900

    feat: delete userId in file table and add delete on cascade for relations

---
+++ prisma/migrations/20260605122115_add_cascade_delete/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `File` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_folderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Folder" DROP CONSTRAINT "Folder_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "File" DROP COLUMN "userId";
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
